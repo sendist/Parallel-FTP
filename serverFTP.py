5fbc48ca73ee36062facbe08bd72701e9f92209d
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2024-06-07 16:49:40 +0700

    update code serverFTP and clientFTP

--- serverFTP.py
+++ serverFTP.py
@@ -35,9 +35,11 @@ class ClientThread(threading.Thread):
                 elif cmd == 'RETR':
                     self.retrieve_file(arg)
                     self.retr_count += 1
+                    self.update_client_operations('RETR')
                 elif cmd == 'STOR':
                     self.store_file(arg)
                     self.stor_count += 1
+                    self.update_client_operations('STOR')
                 elif cmd == 'CWD':
                     self.change_dir(arg)
                 elif cmd == 'QUIT':
@@ -53,12 +55,19 @@ class ClientThread(threading.Thread):
 
     def add_client(self):
         active_clients.append(self.client_address)
-        print(f"Client {self.client_address} connected.")
+        if self.client_address not in client_operations:
+            client_operations[self.client_address] = {'RETR': 0, 'STOR': 0}
+        print(f"Client {self.client_address} connected. Current active clients: {len(active_clients)}")
 
     def remove_client(self):
         if self.client_address in active_clients:
             active_clients.remove(self.client_address)
-        print(f"Client {self.client_address} disconnected.")
+        print(f"Client {self.client_address} disconnected. Current active clients: {len(active_clients)}")
+        
+        # Check if there are no active clients
+        if not active_clients:
+            self.display_most_active_client()
+            self.reset_client_operations()
 
     def list_files(self):
         files = os.listdir(self.cwd)
@@ -110,9 +119,38 @@ class ClientThread(threading.Thread):
     def active_clients(self):
         self.client_socket.send(bytes(str(active_clients), "utf-8"))
 
+    def update_client_operations(self, operation):
+        if self.client_address in client_operations:
+            client_operations[self.client_address][operation] += 1
+            print(f"Client {self.client_address} - {operation} count: {client_operations[self.client_address][operation]}")
+
+    def display_most_active_client(self):
+        if client_operations:
+            max_retr_client = max(client_operations.items(), key=lambda x: x[1]['RETR'])
+            max_stor_client = max(client_operations.items(), key=lambda x: x[1]['STOR'])
+
+            max_retr_clients = [client for client, ops in client_operations.items() if ops['RETR'] == max_retr_client[1]['RETR']]
+            max_stor_clients = [client for client, ops in client_operations.items() if ops['STOR'] == max_stor_client[1]['STOR']]
+
+            if max_retr_client[1]['RETR'] > 0:
+                if len(max_retr_clients) > 1:
+                    print(f"Clients with the same number of downloads (RETR): {', '.join(map(str, max_retr_clients))} with {max_retr_client[1]['RETR']} downloads each")
+                else:
+                    print(f"Client with the most downloads (RETR): {max_retr_client[0]} with {max_retr_client[1]['RETR']} downloads")
+
+            if max_stor_client[1]['STOR'] > 0:
+                if len(max_stor_clients) > 1:
+                    print(f"Clients with the same number of uploads (STOR): {', '.join(map(str, max_stor_clients))} with {max_stor_client[1]['STOR']} uploads each")
+                else:
+                    print(f"Client with the most uploads (STOR): {max_stor_client[0]} with {max_stor_client[1]['STOR']} uploads")
+
+    def reset_client_operations(self):
+        global client_operations
+        client_operations = {}
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_address = ('192.168.0.114', 12345)
+server_address = ('192.168.168.168', 12345)
 print('Starting up on {} port {}'.format(*server_address))
 server_socket.bind(server_address)
 print('Binding successful, now listening...')
@@ -120,6 +158,7 @@ server_socket.listen(5)
 print("FTP Server started...")
 
 active_clients = []
+client_operations = {}
 
 while True:
     print('Waiting for a connection...')
